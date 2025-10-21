a9b92cf4b677695e9d528100d7b0eca9b75a812f
LuisPabloG <3133456430901@ingenieria.usac.edu.gt>
2025-10-20 18:46:26 -0600

    vacio

--- proyecto3/generate_proto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Este script genera los archivos Go desde el proto
-
-mkdir -p proto
-
-protoc --go_out=. --go_opt=paths=source_relative \
-       --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-       ./proto/weathertweet.proto
